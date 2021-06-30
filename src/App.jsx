ed9878272f4548f2d8d40fc277f95bcbddb70958
Alexander <mendelberg44@gmail.com>
2021-06-30 19:22:36 +0300

    options

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './components/header/Header.jsx';
 import SearchFlights from './components/searchFlights/SearchFlights.jsx';
+import Scoreboard from './components/scoreboard/Scoreboard.jsx';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Header />
 			<main className="main">
 				<SearchFlights />
+				<Scoreboard />
 			</main>
 		</>
 	);
