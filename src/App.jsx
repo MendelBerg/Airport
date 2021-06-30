6c5f10ef33224ca3f470a7e50db6ff3b3e6be889
Alexander <mendelberg44@gmail.com>
2021-06-30 14:45:49 +0300

    header done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import Header from './components/header/Header.jsx';
+
+const App = () => {
+  return <Header />;
+};
+
+export default App;
